3f170afb173f2a2f8775fe5fde2800556fb4cb58
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-21 16:40:07 -0500

    window.py: add button 'Click Here' that prints 'Clicked Here' in console

--- window.py
+++ window.py
@@ -3,8 +3,14 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+def on_button_clicked(widget):
+    print("Clicked Here")
+
 window = Gtk.Window()
 window.set_title("Hello World")
+button = Gtk.Button(label = "Click Here")
+window.add(button)
+button.connect("clicked", on_button_clicked)
 window.connect("destroy", Gtk.main_quit)
-window.show()
+window.show_all()
 Gtk.main()
