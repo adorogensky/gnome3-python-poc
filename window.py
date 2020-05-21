2afb8740a643b29ece788fe45bf7688a1d6084cb
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-21 16:00:34 -0500

    windows.py: basic way to display gnome window without adding a class

--- window.py
+++ window.py
@@ -2,23 +2,8 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-import sys
 
-class MyApplication(Gtk.Application):
-
-    def do_activate(self):
-        # create a Gtk Window belonging to the application itself
-        window = Gtk.Window(application=self)
-        # set the title
-        window.set_title("Welcome to GNOME")
-        # show the window
-        window.set_default_size(700, 100)
-        # CENTER, CENTER_ALWAYS and CENTER_ON_PARENT modifiers won't positon window in center
-        window.set_position(Gtk.WindowPosition.CENTER)
-        window.show_all()
-
-# create and run the application, exit with the value returned by
-# running the program
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+window = Gtk.Window()
+window.connect("destroy", Gtk.main_quit)
+window.show()
+Gtk.main()
