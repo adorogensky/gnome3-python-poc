358596d8ea07c1b528b3fb0b55373a13855cc6fe
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-21 17:12:53 -0500

    window.py: create MyWindow class that extends Gtk.Window and define window init and button handler on it

--- window.py
+++ window.py
@@ -3,14 +3,17 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-def on_button_clicked(widget):
-    print("Clicked Here")
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title = "Hello World")
+        button = Gtk.Button(label = "Click Here")
+        self.add(button)
+        button.connect("clicked", self.on_button_clicked)
+    def on_button_clicked(self, widget):
+        print("Clicked Here")
 
-window = Gtk.Window()
-window.set_title("Hello World")
-button = Gtk.Button(label = "Click Here")
-window.add(button)
-button.connect("clicked", on_button_clicked)
+window = MyWindow()
 window.connect("destroy", Gtk.main_quit)
 window.show_all()
 Gtk.main()
+
