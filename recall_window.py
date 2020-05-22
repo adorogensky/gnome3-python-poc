97c5068edb5d07b1ab2509fddcd0496a6003a2c7
Alex Dorogensky <amazinglifetime@gmail.com>
2020-05-22 09:22:08 -0500

    recall_window.py: recall code for basic closable window

---
+++ recall_window.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import gi
+gi.require_version("Gtk", "3.0")
+# print(gi.__file__)
+from gi.repository import Gtk
+
+window = Gtk.Window(title = "Hello World")
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
